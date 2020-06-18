9aea9438ef570b0f439a015f7e753a68fb2b597f
zhangkailiang <zhangkailiang@ultrapower.com.cn>
2020-06-18 09:28:04 +0800

    修改helium组件初始顺序

--- helium-cloud/helium-cloud-starter/src/main/java/org/helium/framework/spring/autoconfigure/HeliumAutoConfiguration.java
+++ helium-cloud/helium-cloud-starter/src/main/java/org/helium/framework/spring/autoconfigure/HeliumAutoConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 
 @Configuration
@@ -30,6 +31,7 @@ public class HeliumAutoConfiguration {
 		return new HeliumHealthIndicator();
 	}
 
+	@DependsOn("heliumHealthIndicator")
 	@Bean
 	public HeliumAutoWired heliumAutoWired() {
 		return new HeliumAutoWired();
