3ede72b606e60c1d30c8e8f75692be508eb551bc
wuhaotx <wuhaotx@feinno.com>
2020-12-15 19:08:02 +0800

    注解开启

--- helium-cloud/helium-cloud-license-starter/src/main/java/org/helium/framework/spring/autoconfigure/LicenseAutoConfiguration.java
+++ helium-cloud/helium-cloud-license-starter/src/main/java/org/helium/framework/spring/autoconfigure/LicenseAutoConfiguration.java
@@ -1,5 +1,6 @@
 package org.helium.framework.spring.autoconfigure;
 
+import org.helium.framework.spring.annotation.EnableLicenseConfiguration;
 import org.helium.framework.spring.controller.LicenseController;
 import org.helium.framework.spring.health.LicenseHealthIndicator;
 import org.helium.framework.spring.service.LicService;
@@ -7,11 +8,13 @@ import org.helium.framework.spring.service.LicServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnBean(annotation = EnableLicenseConfiguration.class)
 @EnableConfigurationProperties(LicenseConfig.class)
 public class LicenseAutoConfiguration {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LicenseAutoConfiguration.class);
