faa3fba029110357bd2c1805f4bb26f36550ec11
wuhaotx <wuhaotx@feinno.com>
2020-12-23 17:00:22 +0800

    删除健康检查

--- helium-cloud/helium-cloud-license-starter/src/main/java/org/helium/framework/spring/autoconfigure/LicenseAutoConfiguration.java
+++ helium-cloud/helium-cloud-license-starter/src/main/java/org/helium/framework/spring/autoconfigure/LicenseAutoConfiguration.java
@@ -1,8 +1,6 @@
 package org.helium.framework.spring.autoconfigure;
 
-//import org.helium.framework.spring.annotation.EnableLicenseConfiguration;
 import org.helium.framework.spring.controller.LicenseController;
-import org.helium.framework.spring.health.LicenseHealthIndicator;
 import org.helium.framework.spring.service.LicService;
 import org.helium.framework.spring.service.LicServiceImpl;
 import org.slf4j.Logger;
@@ -14,16 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ConditionalOnBean(annotation = EnableLicenseConfiguration.class)
 @EnableConfigurationProperties(LicenseConfig.class)
 public class LicenseAutoConfiguration {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LicenseAutoConfiguration.class);
 
-
-	@Bean
-	public LicenseHealthIndicator heliumHealthIndicator() {
-		return new LicenseHealthIndicator();
-	}
 	@Bean
 	public LicService licService() {
 		return new LicServiceImpl();
